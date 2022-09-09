bf5d6aa75450bff7f2cdd15845cfdb29c4182b6e
Georgios Charalampous <gchara14@gmail.com>
2022-09-09 11:24:58 -0700

    Updated sweep transmission for straight waveguide

--- FDTD/waveguide-mode-taper/sweep_transmission/sweep_width_transmission.py
+++ FDTD/waveguide-mode-taper/sweep_transmission/sweep_width_transmission.py
@@ -21,9 +21,10 @@ import lumapi
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import sys 
+import time
 sys.path.append("..")
 
-from waveguide_render import waveguide_draw  
+from waveguide_taper_render import waveguide_taper_draw  
 from user_inputs.user_simulation_parameters import *  
 from user_inputs.user_materials import *
 from user_inputs.user_sweep_parameters import *    
@@ -37,7 +38,7 @@ wav = ((wavelength_start + wavelength_stop)/2)*1e6
 fdtd.save(str(round(wav,2)) + "nm_straight_waveguide_width_sweep" + "_thick_" + str(round(wg_thickness*1e6,2)))
 
 
-waveguide_draw(fdtd)
+waveguide_taper_draw(fdtd)
 add_fdtd_region(fdtd)
 
 fdtd.redrawoff()
@@ -54,8 +55,30 @@ trans_t=[]
 for wd in range(0,len(wg_width_array)):
     fdtd.switchtolayout()    
     fdtd.redrawoff()
-    fdtd.setnamed("waveguide","y span",wg_width_array[wd])
+    
+    wg_width_left = wg_width_array[wd]
+    fdtd.setnamed("waveguide_left","y span",wg_width_left)
+      
+    alpha = (wg_width_left - wg_width_right)/pow(taper_length,m);
+    xspan = np.linspace(0, taper_length, poly_res);
+    yspan = alpha * pow((taper_length - xspan),m) + wg_width_right;
 
+    V = np.zeros((2*poly_res,2))
+
+    for x in range(0,len(xspan)):
+        V[x][0] = xspan[x]
+        V[x][1] = -yspan[x]/2
+        
+    xspan_r = np.flip(xspan, axis=0)
+    yspan_r = np.flip(yspan, axis=0)
+    
+    for x in range(len(xspan),2*len(xspan)):
+        V[x][0] = xspan_r[x-len(xspan)]
+        V[x][1] = yspan_r[x-len(xspan)]/2
+
+    fdtd.setnamed("taper","vertices",V)
+
+    #time.sleep(5)
     fdtd.run()
     trans_f.append(fdtd.getresult("monitor_exp","expansion for input").get("T_forward"))
     trans_t.append(fdtd.getresult("monitor_exp","expansion for input").get("T_total"))
