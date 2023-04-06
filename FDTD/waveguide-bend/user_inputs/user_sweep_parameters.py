621dcd4ccf97cc6f77683c194e003c22f170685a
Georgios <gchara14@gmail.com>
2023-04-06 02:36:21 -0700

    Update Mode Profile

--- FDTD/waveguide-bend/user_inputs/user_sweep_parameters.py
+++ FDTD/waveguide-bend/user_inputs/user_sweep_parameters.py
@@ -20,8 +20,8 @@ The waveguide parameter to sweep bend_radius.
 
 
 bend_radius_start = 10.0e-6     # Choose the start waveguide width 'wg_width_start' you want to start sweeping
-bend_radius_stop = 20.0e-6      # Choose the stop waveguide width 'wg_width_stop' you want to finish sweeping
-bend_radius_step = 5e-6      # Choose the step of each sweep 'wg_step'
+bend_radius_stop = 150.0e-6      # Choose the stop waveguide width 'wg_width_stop' you want to finish sweeping
+bend_radius_step = 10e-6      # Choose the step of each sweep 'wg_step'
 
 
 
