4fe8bfa021a80f910aa0f2691bd247085739379a
gcharalampous <102922093+gcharalampous@users.noreply.github.com>
2023-04-11 14:19:19 -0700

    Update user_sweep_parameters.py

--- MODE/waveguide/user_inputs/user_sweep_parameters.py
+++ MODE/waveguide/user_inputs/user_sweep_parameters.py
@@ -38,15 +38,15 @@ wg_width_start = 0.50e-6     # Choose the start waveguide width 'wg_width_start'
 #you want to start sweeping
 wg_width_stop = 3.0e-6      # Choose the stop waveguide width 'wg_width_stop' you 
 #want to finish sweeping
-wg_width_step = 0.10e-6     # Choose the step of each sweep 'wg_step'
+wg_width_step = 0.10e-6     # Choose the step of each sweep 'wg_width_step'
 
 
 
 
-wg_height_start = 0.01e-6     # Choose the start waveguide width 'wg_width_start' 
+wg_height_start = 0.01e-6     # Choose the start waveguide height 'wg_height_start' 
 #you want to start sweeping
-wg_height_stop = 0.15e-6      # Choose the stop waveguide width 'wg_width_stop' you 
+wg_height_stop = 0.15e-6      # Choose the stop waveguide height 'wg_height_stop' you 
 #want to finish sweeping
-wg_height_step = 0.025e-6     # Choose the step of each sweep 'wg_step'
+wg_height_step = 0.025e-6     # Choose the step of each sweep 'wg_height_step'
 
 
