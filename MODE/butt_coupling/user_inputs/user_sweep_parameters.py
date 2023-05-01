431bae033b70046472ee6ffe55f1e3fe31c4b91d
Georgios <gchara14@gmail.com>
2023-05-01 12:07:24 -0700

    horizontal/vertical misalignment

--- MODE/butt_coupling/user_inputs/user_sweep_parameters.py
+++ MODE/butt_coupling/user_inputs/user_sweep_parameters.py
@@ -36,10 +36,19 @@ user_simulation_paramaters.py
 
 
 
-wg_2_width_start = 2.0e-6    # Choose the start width for waveguide-2 'wg_2_width_start' 
+wg_2_width_start = 0.250e-6    # Choose the start width for waveguide-2 'wg_2_width_start' 
 wg_2_width_stop = 10.0e-6      # Choose the stop width for waveguide-2 'wg_2_width_stop' 
 wg_2_width_step = 0.50e-6     # Choose the step width for waveguide-2 'wg_2_width_step'  
 
 
 
+# Misalign the two beams in y (horizontal) or z (vertical) directions
+misalign_y_start = -5e-6
+misalign_y_stop = 5e-6
+misalign_y_step = 0.5e-6
+
+
+misalign_z_start  = -5e-6
+misalign_z_stop = 5e-6
+misalign_z_step  = 0.25e-6
 
