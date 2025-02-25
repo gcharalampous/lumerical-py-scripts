5ab77dec4c99d95a71017c4fdfd0fdc6ab1e6ae9
Georgios <gchara14@gmail.com>
2025-02-25 15:17:57 -0800

    adding Y branch - draft version

---
+++ FDTD/adiabatic_y_branch/fields/getFields.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# @author: Georgios Gcharalampous (gcharalampous)
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+User-inputs are Not required.
+
+The script plots the E-fields profile for the device structure
+defined in the adiabatic_y_branch.fsp file
+
+"""
+
+
+#----------------------------------------------------------------------------
+# Imports from user input files
+# ---------------------------------------------------------------------------
+
+import numpy as np
+import lumapi, os
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from config import *
+
+# from FDTD.waveguide_cross.override_cross_region import *
+
+
+def getFields(fdtd):
+    
+   
+    field_xy = fdtd.getelectric("field_xy")
+
+    x = fdtd.getdata("field_xy","x").squeeze()
+    y = fdtd.getdata("field_xy","y").squeeze()
+    
+    return x,y,field_xy
+
+
+
+
+
+
+if(__name__=="__main__"):
+    with lumapi.FDTD(FDTD_ADIAB_Y_BR_DIRECTORY_READ) as fdtd:
+        
+# ------------ Comment for Avoiding Overriding the Simulation Region
+        # override_cross(fdtd=fdtd)
+        # fdtd.run()
+        
+        x,y,E_xy = getFields(fdtd=fdtd)
+        c_wavelength = np.rint(len(E_xy[0,0,0,:])/2)
+        px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+        
+# --------------------------------Top-View---------------------------------
+        fig, ax = plt.subplots(figsize=(512*px, 256*px))
+        cmap = ax.pcolormesh(x*1e6, y*1e6, (np.transpose(E_xy[:, :, 0, int(c_wavelength)])),
+                     shading='gouraud', cmap='jet', norm=LogNorm(vmin=1e-4, vmax=1))
+        fig.colorbar(cmap)
+        plt.xlabel("x (um)")
+        plt.ylabel("y (um)")
+        plt.title('Top-view(xy)')
+        plt.tight_layout()
+        file_name_plot = os.path.join(FDTD_ADIAB_Y_BR_DIRECTORY_WRITE[2], "E_profile_xy.png")
+        plt.savefig(file_name_plot)
+        
+
+        
+        plt.show()
+        
+        
---
+++ FDTD/adiabatic_y_branch/transmission/getFrequencyResponse.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# @author: Georgios Gcharalampous (gcharalampous)
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+User-inputs are Not required.
+
+The script plots the transmission for the waveguide adiabatic y-branch
+structure defined in the adiabatic_y_branch.fsp file
+from the 'Ttop' and 'Tbot' monitor ports.
+
+"""
+
+#----------------------------------------------------------------------------
+# Imports from user input files
+# ---------------------------------------------------------------------------
+
+import numpy as np
+import lumapi, os
+import matplotlib.pyplot as plt
+import scipy.constants as scpy
+from config import *
+
+# from FDTD.waveguide_cross.override_cross_region import *
+
+def getCrossResponse(fdtd):
+    T1  = np.squeeze(fdtd.getresult("T_top","T").get("T"))
+    T2  = np.squeeze(fdtd.getresult("T_bot","T").get("T"))
+    f  = np.squeeze(fdtd.getdata("T_top","f"))
+
+    return T1, T2, f
+
+
+
+if(__name__=="__main__"):
+    with lumapi.FDTD(FDTD_ADIAB_Y_BR_DIRECTORY_READ) as fdtd:
+        
+# ------------ Comment for Avoiding Overriding the Simulation Region
+        # override_cross(fdtd=fdtd)
+        # fdtd.run()
+
+# --------------------------------Plot-T/R---------------------------------
+
+        T1, T2, f = getCrossResponse(fdtd=fdtd)
+
+# --------------------------------Plot-T/R---------------------------------
+
+        px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+        fig, ax = plt.subplots(figsize=(512*px, 256*px))
+        ax.plot((scpy.c/f)*1e6,T1,label = 'Port 1')
+        ax.plot((scpy.c/f)*1e6,abs(T2),label = 'Port 2',linestyle='--')
+        ax.set_ylim(0,1)
+        ax.grid(which='both')
+        ax.legend()
+        ax.set_xlabel("wavelength (um)")
+        ax.set_ylabel("Magnitude")
+        plt.tight_layout()
+        file_name_plot = os.path.join(FDTD_ADIAB_Y_BR_DIRECTORY_WRITE[1], "frequency_response.png")
+        plt.savefig(file_name_plot)        
+        
+        fig, ax = plt.subplots(figsize=(512*px, 256*px))
+        ax.plot((scpy.c/f)*1e6,10*np.log10(T1),label = 'Port 1')
+        ax.plot((scpy.c/f)*1e6,10*np.log10(abs(T2)),label = 'Port 2',linestyle='--')
+        ax.grid(which='major')
+        ax.legend()
+        ax.set_xlabel("wavelength (um)")
+        ax.set_ylabel("Magnitude (dB)")
+        
+        min_val = 10 * np.log10(np.min([T1, abs(T2)]))
+        max_val = 10 * np.log10(np.max([T1, abs(T2)]))
+        ax.axhline(y=min_val, color='black', linestyle='--')
+        ax.axhline(y=max_val, color='black', linestyle='--')
+        
+        IL =  (-3 - max_val)
+        deviation_from_3dB = (max_val - min_val) - IL
+        
+        print(f"Deviation: {deviation_from_3dB} dB")
+        deviation_percentage = (10 ** (deviation_from_3dB / 10) - 1) * 100
+        print(f"Deviation: {deviation_percentage:.2f}%")
+
+        
+        plt.tight_layout()
+        file_name_plot = os.path.join(FDTD_ADIAB_Y_BR_DIRECTORY_WRITE[1], "frequency_response_dB.png")
+        plt.savefig(file_name_plot)      
+        
+        plt.show()
