5123d1b6cddf2743dd5f01f5aa1ee2542bd8698e
Georgios Charalampous <gcharalampous@ucdavis.edu>
2026-02-28 21:27:00 -0800

    Add getFrequencyResponse script for plotting waveguide transmission results

---
+++ FDTD/waveguide_bend/transmission/getFrequencyResponse.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# @author: Georgios Gcharalampous (gcharalampous)
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+User-inputs are Not required.
+
+The script plots the transmission for the fundamental TE or TM mode
+of the waveguide bend structure defined in the circular_bend.fsp or
+euler_bend.fsp file from the T_exp monitor. The back reflection from
+the source is also printed.
+
+"""
+
+#----------------------------------------------------------------------------
+# Imports from user input files
+# ---------------------------------------------------------------------------
+
+import numpy as np
+import lumapi
+import matplotlib.pyplot as plt
+import scipy.constants as scpy
+from pathlib import Path
+from project_layout import setup
+from FDTD.waveguide_bend.user_inputs.user_simulation_parameters import file_index
+
+
+def getFrequencyResponse(fdtd):
+    fdtd.run()
+    T_total  = np.squeeze(fdtd.getresult("T_exp","expansion for T").get("T_total"))
+    T_forward  = np.squeeze(fdtd.getresult("T_exp","expansion for T").get("T_forward"))
+    f  = np.squeeze(fdtd.getdata("transmission","f"))
+
+    return T_total, T_forward, f
+
+
+
+if(__name__=="__main__"):
+    spec, out, templates = setup("fdtd.waveguide_bend", __file__)
+    template_fsp = templates[file_index]
+    figures_dir = out["figures"] / "Transmission"
+    figures_dir.mkdir(parents=True, exist_ok=True)
+    
+    with lumapi.FDTD(str(template_fsp)) as fdtd:
+        
+
+# -----------------------------Plot-T_forward/T_total------------------------------
+
+        T_total, T_forward, f = getFrequencyResponse(fdtd=fdtd)
+        px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+
+# -----------------------------Plot-T_forward/T_total------------------------------
+
+        fig, ax = plt.subplots(figsize=(512*px, 256*px))
+        ax.plot((scpy.c/f)*1e6, T_total,'-o',label = 'Total')
+        ax.plot((scpy.c/f)*1e6, T_forward,'-o',label = 'Fundamental')
+        ax.legend()
+        ax.set_xlabel("wavelength (um)")
+        ax.set_ylabel("Magnitude")
+        plt.ylim([0,1])
+        plt.tight_layout()
+        plt.savefig(figures_dir / "frequency_response.png")
+
+        fig, ax = plt.subplots(figsize=(512*px, 256*px))
+        ax.plot((scpy.c/f)*1e6,10*np.log10(T_total),'-o',label = 'Total')
+        ax.plot((scpy.c/f)*1e6,10*np.log10(abs(T_forward)),'-o',label = 'Fundamental')
+        ax.legend()
+        ax.set_xlabel("wavelength (um)")
+        ax.set_ylabel("Magnitude (dB)")
+        plt.tight_layout()
+        plt.savefig(figures_dir / "frequency_response_dB.png")
+
+        plt.show()
+        
+        # print('Back Reflection: ' + str(round(10*np.log10(abs(R.mean())),2)) + ' dB')
